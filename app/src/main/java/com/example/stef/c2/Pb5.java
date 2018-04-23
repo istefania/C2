34dfaf10d1c28ffda540299afe1decdeb1523b88
Stefania Ionescu <ionescu01_stefania@yahoo.ro>
2018-04-23 20:41:39 +0300

    add Pb5

---
+++ app/src/main/java/com/example/stef/c2/Pb5.java
@@ -0,0 +1,13 @@
+package com.example.stef.c2;
+
+public class Pb5 {
+    public static void main(String[] args){
+
+        printSubstring("mama");
+    }
+
+    static void printSubstring(String a){
+
+        System.out.println(a.substring(1,3));
+    }
+}
